283840ee46b483a1d74cf883839d33a78fd2a9e6
masroor508 <83511845+masroor508@users.noreply.github.com>
2023-03-30 00:17:22 +0530

    Update applicationstart.sh

--- applicationstart.sh
+++ applicationstart.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Change to the application directory
-cd /home/ubuntu/
-
-# Install application dependencies
-npm install
-
-# Start the Node.js application with PM2
-pm2 start -f server.js
+cd /home/ubuntu
+pm2 -f start server.js
