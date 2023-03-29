1b36fd8c29835d9fa415bcb822b26bfeaa770e3e
masroor508 <83511845+masroor508@users.noreply.github.com>
2023-03-29 12:53:21 +0530

    Update beforeinstall.sh

--- beforeinstall.sh
+++ beforeinstall.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# Update package manager
+sudo apt-get update
+
+# Install Node.js
+sudo apt-get install -y nodejs
+
+# Install npm (Node Package Manager)
+sudo apt-get install -y npm
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+
+# Install PM2 process manager globally
+sudo npm install -g pm2
